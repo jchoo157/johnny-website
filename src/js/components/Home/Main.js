b27a06d638f7849168d0d942adc2ebc34552d663
Johnny Choo <jchoo156@gmail.com>
2018-05-31 14:21:28 -0700

    create Main component with basic style

--- src/js/components/Home/Main.js
+++ src/js/components/Home/Main.js
@@ -4,8 +4,12 @@ export default class Main extends Component {
   render() {
     return(
       <div id="main">
-        <h1>Hi, i'm Johnny!!!</h1>
-        <p>I am a Web Developer always interested in learning new things!</p>
+        <div className="intro">
+          <h1>Hi, i'm Johnny!!!</h1>
+          <p>I am a Web Developer always interested in learning new things!</p>
+        </div>
+        <div className="choo-picture">
+        </div>
       </div>
     )
   }
