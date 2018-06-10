67f67c90f09a30f00b7819c1319dbe857a70e1f5
Johnny Choo <jchoo156@gmail.com>
2018-06-09 17:27:09 -0700

    update text

--- src/js/components/Home/Contact.js
+++ src/js/components/Home/Contact.js
@@ -7,7 +7,7 @@ export default class Contact extends Component {
       <section id="contact">
         <div className="contact-content">
           <h1> Best ways to reach me! </h1>
-          <p>Please feel free to message me through Linkedin or my email address with any questions you may have. I'm always happy to hear about new opportunities and what you have to offer!</p>
+          <p>Please feel free to message me through Linkedin or send me an email with any questions you may have. I'm always happy to hear about new opportunities and what you have to offer!</p>
           <div className="container">
             <div className="contact-linkedin">
               <a href="https://www.linkedin.com/in/jchoo157" target="_blank"><img src="../../../../public/images/icons/linkedin-big-icon.png"/></a>
